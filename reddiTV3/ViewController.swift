6b51feb60eb8554c2768040b0d20bd866bf26307
Jonathan Yu <jonathan@jonathanyu.net>
2015-09-13 13:13:07 -0700

    Initial Commit

---
+++ reddiTV3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  reddiTV3
+//
+//  Created by Jonathan Yu on 9/13/15.
+//  Copyright © 2015 Jonathan Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ reddiTV3Tests/reddiTV3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  reddiTV3Tests.swift
+//  reddiTV3Tests
+//
+//  Created by Jonathan Yu on 9/13/15.
+//  Copyright © 2015 Jonathan Yu. All rights reserved.
+//
+
+import XCTest
+@testable import reddiTV3
+
+class reddiTV3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
